8ddd01bf50c69f11c417fb42e02eb14a77e6d580
AnanyaDasgupta <ananya.dasgupta@one2n.in>
2026-04-24 13:23:05 +0530

    refactor(student-api): move system endpoints out of main app initialization

--- student-api/app/main.py
+++ student-api/app/main.py
@@ -1,16 +1,7 @@
 from fastapi import FastAPI
-from app.api.v1.student_routes import router as student_router
-import app.core.logger
+
+from app.routes.system_routes import router as system_router
+
 
 app = FastAPI()
-
-@app.get("/")
-def root():
-    return {"message": "Welcome to the Student API!"}
-
-@app.get("/healthcheck")
-def healthcheck():
-    return {"status": "ok"}
-
-# Include the student router to handle all student-related endpoints under the /api/v1/students prefix
-app.include_router(student_router)
+app.include_router(system_router)
